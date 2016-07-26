202276d62e7d0f04b46fd3a4bf266d10caba47b3
Muhammet Parlak <muhammetpparlak@gmail.com>
2016-07-26 16:03:49 +0300

    Update LinqToOrderExtensions.cs

--- LinqToOrderExtensions.cs
+++ LinqToOrderExtensions.cs
@@ -6,7 +6,7 @@ using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace EPlatform.CRM.Business.Utils
+namespace LinqToOrderExtensions
 {
     public static class LinqToOrderExtensions
     {
